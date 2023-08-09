da3c5d375995897b316a7875096a481c9db59613
Daniel Andres <ing.dhguissepe@gmail.com>
2023-08-09 18:00:38 -0500

    update watchdog script

--- shell/watchdog_example.sh
+++ shell/watchdog_example.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-# Change the host for the one you want to check.
-sudo tcpdump -#XXtttt -G 60 -C 2 -w /tmp/reaching_google_https.pcap host www.google.com and port 443
+# Change the host for the one you want to check. It can check against IPs as well.
+# The parameter for -G is the time it preserves the packet capture file, in seconds, before wiping it out.
+# The parameter for -C is the maximum size of the file in millions of bytes. For example: 2 would be 2.000.000 bytes.
+
+sudo tcpdump -#XXtttt -G 600 -C 2 -w /tmp/reaching_google_https.pcap host www.google.com and port 443
